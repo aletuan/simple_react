e05220409cf05d1051446c7cd5575e81a0113eb7
Tuan Anh Le <anhlt1983@gmail.com>
2018-01-21 08:44:57 +0900

    Adding babel to transform jsx

---
+++ static/App.js
@@ -0,0 +1,7 @@
+var componentNode = document.getElementById("content");
+var component = React.createElement(
+  "h1",
+  null,
+  "Hello, World"
+);
+ReactDOM.render(component, componentNode);
---
+++ static/App.jsx
@@ -0,0 +1,3 @@
+var componentNode = document.getElementById("content");
+var component = <h1>Hello, World</h1>;
+ReactDOM.render(component, componentNode);
