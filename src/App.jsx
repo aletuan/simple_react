210387a3b6717cdb2684b77a2cf795b45a6c6854
Tuan Anh Le <anhlt1983@gmail.com>
2018-01-21 08:54:54 +0900

    Code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 var componentNode = document.getElementById("content");
-var component = <h1>Hello, World !</h1>;
+var component = <h1>Hello, World ! Hello Again.</h1>;
 ReactDOM.render(component, componentNode);
