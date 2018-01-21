96de104c6426398466b043709323397ec78de0ef
Tuan Anh Le <anhlt1983@gmail.com>
2018-01-21 09:41:21 +0900

    Add new, componse new component class

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,41 @@ const message = continents.map(c => `Hello ${c}!`).join(',');
 var component = <h1>{message}</h1>;
 */
 
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the Issue Filter</div>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the table of Issues</div>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the Issue Add entry form</div>
+        );
+    }
+}
+
 class IssueList extends React.Component {
     render() {
         return (
-            <div>This is a placeholder for the issue list</div>
+            <div>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
         );
     }
 }
