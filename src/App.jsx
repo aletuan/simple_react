0c907b85307530ea1ade03f05fe16aa9bb276793
Tuan Anh Le <anhlt1983@gmail.com>
2018-01-21 09:26:06 +0900

    create react class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-const componentNode = document.getElementById("content");
+const contentNode = document.getElementById("content");
 
+/*
 const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
 const message = continents.map(c => `Hello ${c}!`).join(',');
 
 var component = <h1>{message}</h1>;
-ReactDOM.render(component, componentNode);
+*/
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue list</div>
+        );
+    }
+}
+
+//ReactDOM.render(component, componentNode);
+ReactDOM.render(<IssueList />, contentNode);
