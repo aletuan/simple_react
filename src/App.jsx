066da744fe23a00c98d07e5cc30f7a06ec525175
Tuan Anh Le <anhlt1983@gmail.com>
2018-01-21 09:12:15 +0900

    add es2015 features

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-var componentNode = document.getElementById("content");
-var component = <h1>Hello, World ! Hello Again.</h1>;
+const componentNode = document.getElementById("content");
+
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const message = continents.map(c => `Hello ${c}!`).join(',');
+
+var component = <h1>{message}</h1>;
 ReactDOM.render(component, componentNode);
--- static/App.js
+++ static/App.js
@@ -1,7 +1,15 @@
+'use strict';
+
 var componentNode = document.getElementById("content");
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var message = continents.map(function (c) {
+  return 'Hello ' + c + '!';
+}).join(',');
+
 var component = React.createElement(
-  "h1",
+  'h1',
   null,
-  "Hello, World ! Hello Again."
+  message
 );
 ReactDOM.render(component, componentNode);
